ee294c0b767dd7fec405f5cd2f4c965dcd00b88d
zhao <1254042169@qq.com>
2019-09-06 19:03:05 +0800

    @dev 删除注释:see_no_evil:

--- extension.js
+++ extension.js
@@ -16,18 +16,13 @@ function isExistFile(file) {
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function provideDefinition(document, position, token) {
-    console.log("====== 进入 provideDefinition 方法 ======");
     const fileName = document.fileName;
     let workDir = path.dirname(fileName);
     const word = document.getText(document.getWordRangeAtPosition(position));
     const line = document.lineAt(position);
 
     console.log("fileName: ", fileName); // 当前文件完整路径
-    console.log("workDir: ", workDir); // 当前文件所在目录
-    console.log("word: ", word); // 当前光标所在单词
     console.log("line: ", line.text); // 当前光标所在行
-    // 只处理package.json文件
-    console.log(word, line.text);
     // 简写
     let aliasConf = {
         "@": "src",
